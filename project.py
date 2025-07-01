ff170bb52fe9938736a7e7d7fb87f3fb4075e16c
Layr Bastos <102252629+LayrBastos@users.noreply.github.com>
2025-07-01 00:12:42 -0300

    Added 'play again' feature

--- project.py
+++ project.py
@@ -18,6 +18,13 @@ def select_level(n):
         return 10
     
 
+def play_again():
+    while True:
+        restart = input("Play Again?:\n(Y) - YES\n(N) - NO\n>>>").lower()
+        if restart in ('y', 'n'):
+            return restart == 'y'
+
+
 def height(h):
     """Converts the Pokémon's height to meters or centimeters (if less than 1 m)
 
@@ -47,65 +54,73 @@ def weight(w):
 
 
 def main():
-    pokemon_id = random.randint(1, 150)
-    pk = pb.pokemon('pikachu') # substitute 'pokemon_id' for 'pikachu' when recording the presentation video
-    species = pb.pokemon_species('pikachu') # substitute 'pokemon_id' for 'pikachu' when recording the presentation video
-    hidden = Pokemon(pk.name,
-                     species.color.name,
-                     pk.height,
-                     pk.weight,
-                     pk.types[0].type.name)
-    # Level Selection
-    while True:
-        level = input("Select level:\n(1) - Easy\n(2) - Hard\n>>> ")
-        if level in ('1', '2'):
-            level = int(level)
-            break
-        else:
-            print("Invalid Input.")
-        
-    guesses = 0
-    max_guesses = select_level(level)
+    play_game = True
+    while play_game:
+        pokemon_id = random.randint(1, 150)
+        pk = pb.pokemon('pikachu') # substitute 'pokemon_id' for 'pikachu' when recording the presentation video
+        species = pb.pokemon_species('pikachu') # substitute 'pokemon_id' for 'pikachu' when recording the presentation video
+        hidden = Pokemon(pk.name,
+                         species.color.name,
+                         pk.height,
+                         pk.weight,
+                         pk.types[0].type.name)
+        # Level Selection
+        while True:
+            level = input("Select level:\n(1) - Easy\n(2) - Hard\n>>> ")
+            if level in ('1', '2'):
+                level = int(level)
+                break
+            else:
+                print("Invalid Input.")
 
-    while guesses < max_guesses:
-        if guesses > 0:
-            print(f"Guesses: {guesses}\n")
-            print("+-" * 20)
-        guess = input("Guess who I am: ").lower()
-        try:
-            guessed_pokemon = pb.pokemon(guess)
-            guessed_species = pb.pokemon_species(guess)
-            pkmn = Pokemon(guessed_pokemon.name,
-                           guessed_species.color.name,
-                           guessed_pokemon.height,
-                           guessed_pokemon.weight,
-                           guessed_pokemon.types[0].type.name)
-        except AttributeError:
-            print(f"{guess}...? I don't know that pokemon... Check the spelling and try again.")
-            continue
-        else:
-            guesses += 1
-            
-        if hidden.check_guess(pkmn):
+        guesses = 0
+        max_guesses = select_level(level)
+
+        while guesses < max_guesses:
+            win = False
+            if guesses > 0:
+                print(f"Guesses: {guesses}\n")
+                print("+-" * 20)
+            guess = input("Guess who I am: ").lower()
+            try:
+                guessed_pokemon = pb.pokemon(guess)
+                guessed_species = pb.pokemon_species(guess)
+                pkmn = Pokemon(guessed_pokemon.name,
+                               guessed_species.color.name,
+                               guessed_pokemon.height,
+                               guessed_pokemon.weight,
+                               guessed_pokemon.types[0].type.name)
+            except AttributeError:
+                print(f"{guess}...? I don't know that pokemon... Check the spelling and try again.")
+                continue
+            else:
+                guesses += 1
+
+            if hidden.check_guess(pkmn):
+                win = True
+                break      
+            else:
+                print(f"{pkmn.name} is {height(pkmn.height)} tall and weighs {weight(pkmn.weight)}. So...")
+                if hidden.is_taller_than(pkmn):
+                    print("I am taller and", end=' ')
+                else:
+                    print("I am shorter and", end=' ')
+                if hidden.is_heavier_than(pkmn):
+                    print(f"heavier than {pkmn.name}")
+                else:
+                    print(f"lighter than {pkmn.name}")
+                if not hidden.type_revealed:
+                    hidden.compare_types(pkmn)
+                if not hidden.color_revealed:
+                    hidden.compare_colors(pkmn)
+        if win:
             print(f"You're right!! I am {pkmn.name}!!!")
             print(f"You found me with {guesses} guesses")
-            sys.exit("Thanks for playing.")
         else:
-            print(f"{pkmn.name} is {height(pkmn.height)} tall and weighs {weight(pkmn.weight)}. So...")
-            if hidden.is_taller_than(pkmn):
-                print("I am taller and", end=' ')
-            else:
-                print("I am shorter and", end=' ')
-            if hidden.is_heavier_than(pkmn):
-                print(f"heavier than {pkmn.name}")
-            else:
-                print(f"lighter than {pkmn.name}")
-            if not hidden.type_revealed:
-                hidden.compare_types(pkmn)
-            if not hidden.color_revealed:
-                hidden.compare_colors(pkmn)
-    
-    print(f"You lost... I'm {hidden.name}")
+            print(f"You lost... I'm {hidden.name}")
+
+        play_game = play_again()
+
     sys.exit("Thanks for playing!!! :)")
 
 
